792ce48669f21ea9f65d1290a83908f021e648cf
Heal-Like-Phoenix <open.phoenixproject@gmail.com>
2024-02-24 20:44:59 +0530

    inital commit

---
+++ DB/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+# FastAPI app instance
+app = FastAPI()
+
+
+@app.get("/")
+def hello():
+    return {"Hello": "World"}
+
+
+# Run Uvicorn server
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
